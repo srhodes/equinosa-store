13903d61b41cef5f961320b8f9c2cddd774fddc8
srhodes <amplifier339@comcast.net>
2023-05-24 03:48:22 -0700

    change contact page

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -117,7 +117,7 @@ class Contact extends Component {
                     </div>
                     <div className="col">
                         <a role="button" className="btn btn-link" href="tel:+12065551234"><i className="fa fa-phone" /> 1-206-555-1234</a><br />
-                        <a role="button" className="btn btn-link" href="mailto:fakeemail@fakeemail.co"><i className="fa fa-envelope-o" /> campsites@nucamp.co</a>
+                        <a role="button" className="btn btn-link" href="mailto:fakeemail@fakeemail.co"><i className="fa fa-envelope-o" /> help@equinosa.co</a>
                     </div>
                 </div>
             {/* ADDING CONTACT FORM */}
